4d468a4615f60b705dc283511720e0d78c61eb15
Stven5490 <est.caceres@gmail.com>
2023-05-04 23:47:45 -0400

    Termino de guia

--- src/ejercicios/Ejercicio_4.java
+++ src/ejercicios/Ejercicio_4.java
@@ -8,12 +8,20 @@ el resultado del conteo y además un mensaje que ha salido del bucle.
 public class Ejercicio_4 {
     public static void main(String[] args) {
         System.out.println("Inicio contador");
-        for(int count=0; count<=10;count++){
-            if(count==8) {
+        for (int count = 0; count <= 250; count++) {
+            if (count > 250) {
                 break;
             }
             System.out.println("Count is ==> " + count);
         }
         System.out.println("Has salido del bucle");
+        System.out.println("Empieza bucle desde: 250");
+        for (int count = 250; count <= 1000; count+=10){
+            if (count > 1000){
+                break;
+            }
+            System.out.println("Count is ==> " + count);
+        }
+        System.out.println("Has salido del bluce");
     }
 }
--- src/ejercicios/Ejercicio_6.java
+++ src/ejercicios/Ejercicio_6.java
@@ -2,6 +2,11 @@ package ejercicios;
 //Calcular la suma de los 100 primeros números positivos. (1+2+3+4+5…+100).
 public class Ejercicio_6 {
     public static void main(String[] args) {
-
+        for(int suma = 1; suma <= 100; suma+=1){
+            if (suma > 100){
+                break;
+            }
+            System.out.println("Suma:"+ suma);
+        }
     }
 }
