7a70d8eb002b959f8988a9d1786755eec0c56a45
Stven5490 <est.caceres@gmail.com>
2023-05-14 12:51:06 -0400

    Repaso Programacion Orientada a Objetos 'POO'

--- src/POO/APP.java
+++ src/POO/APP.java
@@ -2,8 +2,19 @@ package POO;
 
 public class APP {
     public static void main(String[] args) {
-        int edad = 10;
-        persona p1 = new Persona();
+
+        Persona p1 = new Persona();
+        Persona p2 = new Persona();
+
         Producto prod1 = new Producto();
+
+        p1.nombre = "Diego";
+        p1.edad = 15;
+        p1.dinero = 1000;
+
+        prod1.nombre = "Papas Lays";
+        prod1.stock = 10;
+        prod1.precio = 800;
+        prod1.descripcion = "Papas Lays corte americano 30 grms.";x
     }
 }
--- src/POO/Producto.java
+++ src/POO/Producto.java
@@ -1,11 +1,18 @@
 package POO;
 
 public class Producto {
-    String nombre;
+    private String nombre;
     int precio;
     String descripcion;
+    Marca marca;
 
     public void disminuirStockEn1(){
         stock = stock -1;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String, nombre){
+        this.nombre = nombre;
     }
 }
